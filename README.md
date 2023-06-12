2b94473e36e49ec9decd718681f2a1996ea9cff2
Khosasih <adammaulanakh1@gmail.com>
2023-06-12 19:41:34 +0700

    Upload file VueJS

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# TokoOnline
-Pembelajaran layouting dengan membangun website menggunakan Vue JS
+# vue-learning-coding
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <HeaderCarousel />
+    <FrontCarousel/>
+    <MidCarousel/>
+    <SocmedCarousel/>
+    <PatnerCarousel/>
+    <FooterCarousel/>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import FrontCarousel from '@/components/FrontCarousel.vue'
+import HeaderCarousel from '@/components/HeaderCarousel.vue'
+import MidCarousel from '@/components/MidCarousel.vue'
+import SocmedCarousel from '@/components/SocmedCarousel.vue'
+import PatnerCarousel from '@/components/PatnerCarousel.vue'
+import FooterCarousel from '@/components/FooterCarousel.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+    // eslint-disable-next-line vue/no-unused-components
+    HeaderCarousel,
+    FrontCarousel,
+    MidCarousel,
+    // eslint-disable-next-line vue/no-unused-components
+    SocmedCarousel,
+    // eslint-disable-next-line vue/no-unused-components
+    PatnerCarousel,
+    // eslint-disable-next-line vue/no-unused-components
+    FooterCarousel
+  }
+}
+</script>
